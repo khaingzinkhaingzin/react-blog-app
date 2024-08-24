73a1a934941ae25b87b514afa56fd2da47e5f837
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-08-24 14:08:31 +0630

    loading in PostList component

--- blog-frontend/src/components/PostList.jsx
+++ blog-frontend/src/components/PostList.jsx
@@ -1,22 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useTransition } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 import axiosInstance from '../axios';
 
 import Post from './Post';
 
 const PostList = () => {
     const [ posts, setPosts ] = useState([]);
+    // const [ isPending, startTransition ] = useTransition();
+    const [ isLoading, setIsLoading ] = useState(false);
 
     useEffect(() => {
-        axiosInstance.get('/posts')
-            .then(response => setPosts(response.data))
-            .catch(error => console.error(error));
+        // startTransition(() => {
+        //     setTimeout(() => {
+        //         axiosInstance.get('/posts')
+        //             .then(response => {
+        //                 setPosts(response.data);
+        //             })
+        //             .catch(error => console.error(error));
+        //     }, 5000);
+        // });
+
+        setIsLoading(true);
+        setTimeout(() => {
+            axiosInstance.get('/posts')
+                .then(response => {
+                    setPosts(response.data);
+                    setIsLoading(false);
+                })
+                .catch(error => {
+                    console.error(error);
+                    setIsLoading(false);
+                });
+        }, 1000);
+
     }, []);
 
     return (
         <section>
-            {posts.map(post => (
+            { 
+                isLoading 
+                && 
+                <Box sx={{ display: 'flex' }}>
+                    <CircularProgress />
+                </Box>
+            }
+            
+            { posts.map(post => (
                 <Post key={post.id} post={post} />
-            ))}
+            )) }
         </section>
     );
 };
